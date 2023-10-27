88b4ce3aca14b16a9bd78d1c9190f914da50dca8
haadibolotbekov <ebashit.na@gmail.com>
2023-10-27 23:59:35 +0300

    added week3 readme link

--- week2/main/java/model/Product.java
+++ week2/main/java/model/Product.java
@@ -7,4 +7,55 @@ public class Product {
     private Integer barcode;
     private Integer price;
 
+    public Product() {
+    }
+
+    public Product(Long id, String name, Integer barcode, Integer price) {
+        this.id = id;
+        this.name = name;
+        this.barcode = barcode;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(Integer barcode) {
+        this.barcode = barcode;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", barcode=" + barcode +
+                ", price=" + price +
+                '}';
+    }
 }
